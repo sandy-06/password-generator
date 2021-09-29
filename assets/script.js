4173eb57f942312bfd168a9ac19e969ba6f1ec04
sandy <sandy-sanders@msn.com>
2021-09-29 10:12:48 -0700

    get input types

--- assets/script.js
+++ assets/script.js
@@ -8,15 +8,26 @@ console.log(alphabet);
 console.log(ucAlphabet);
 console.log(numbers);
 console.log(special);
+
 function getPasswordOptions() {
-  var length= parseInt(prompt("How many Characters do you want in your password"));
-  if(Number.isNaN(length)){
+  var length = parseInt(prompt("Please choose a length between 8 and 128"));
+  if (Number.isNaN(length)) {
     alert("Please enter a number")
-
   }
-  if length< 8
- 
+  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be between 8 and 128. How many characters do you want?"));
 
+  var lcase = confirm("Do you want lowercase letters?");
+  var ucase = confirm("Do you want uppercase letters?");
+  var num = confirm("Do you want numbers?");
+  var symbols = confirm("Do you want special characters?");
+
+  While (!lcase && !ucase && !num && !symbols) {
+    alert("You need to choose at least one option!");
+    lcase = confirm("Do you want lowercase letters?");
+    ucase = confirm("Do you want uppercase letters?");
+    num = confirm("Do you want numbers?");
+    symbols = confirm("Do you want special characters?");
+  };
 }
 getPasswordOptions();
 // Write password to the #password input
@@ -26,7 +37,7 @@ function writePassword() {
 
   passwordText.value = password;
 
- 
+
 
 
 
