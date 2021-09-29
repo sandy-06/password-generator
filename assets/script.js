6a482826f3248da5b4d48b5a04b025fee12b9646
sandy <sandy-sanders@msn.com>
2021-09-29 12:04:56 -0700

    input

--- assets/script.js
+++ assets/script.js
@@ -15,38 +15,39 @@ function getPasswordOptions() {
     alert("Please enter a number")
   }
   while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be between 8 and 128. How many characters do you want?"));
-
+if 
   var lcase = confirm("Do you want lowercase letters?");
   var ucase = confirm("Do you want uppercase letters?");
   var num = confirm("Do you want numbers?");
   var symbols = confirm("Do you want special characters?");
 
-  While (!lcase && !ucase && !num && !symbols) {
+
+ // While(!lcase && !ucase && !num && !symbols) {
     alert("You need to choose at least one option!");
     lcase = confirm("Do you want lowercase letters?");
     ucase = confirm("Do you want uppercase letters?");
     num = confirm("Do you want numbers?");
     symbols = confirm("Do you want special characters?");
   };
-}
-getPasswordOptions();
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  getPasswordOptions();
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-
+    passwordText.value = password;
 
 
 
 
 
 
-}
+
+
+  }
 
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
