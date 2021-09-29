0deaa53a8966a97b1dbbca73469bb781aafc2a20
sandy <sandy-sanders@msn.com>
2021-09-28 19:00:33 -0700

    started vif on variable

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+var ucAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var special = ["@", "%", "+", "!", "#", "$", "^", "?", ":", ",", "(", ")", "/", "{", "}", "[", "]", "~"];
+console.log(alphabet);
+console.log(ucAlphabet);
+console.log(numbers);
+console.log(special);
+function getPasswordOptions() {
+  var length= parseInt(prompt("How many Characters do you want in your password"));
+  if(Number.isNaN(length)){
+    alert("Please enter a number")
 
+  }
+  if length< 8
+ 
+
+}
+getPasswordOptions();
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,10 +26,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-  var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-  var ucAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-  var numbers = numbers[1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  var special = special["@", "%", "+", "!", "#", "$", "^", "?", ":", ",", "(", ")", "{", "}", "[", "]", "~"];
+ 
 
 
 
