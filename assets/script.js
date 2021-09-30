250e1716bc8c4fd686b1b425e4ac9d7c1e7c7c95
sandy <sandy-sanders@msn.com>
2021-09-29 18:01:04 -0700

    final

--- assets/script.js
+++ assets/script.js
@@ -13,41 +13,82 @@ function getPasswordOptions() {
   var length = parseInt(prompt("Please choose a length between 8 and 128"));
   if (Number.isNaN(length)) {
     alert("Please enter a number")
+    return;
   }
   while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be between 8 and 128. How many characters do you want?"));
-if 
-  var lcase = confirm("Do you want lowercase letters?");
-  var ucase = confirm("Do you want uppercase letters?");
-  var num = confirm("Do you want numbers?");
-  var symbols = confirm("Do you want special characters?");
 
 
- // While(!lcase && !ucase && !num && !symbols) {
-    alert("You need to choose at least one option!");
-    lcase = confirm("Do you want lowercase letters?");
-    ucase = confirm("Do you want uppercase letters?");
-    num = confirm("Do you want numbers?");
-    symbols = confirm("Do you want special characters?");
+  var lcase = window.confirm("Do you want lowercase letters? Select 'ok' for yes and 'cancel' for no.");
+
+  var ucase = window.confirm("Do you want uppercase letters? Select 'ok' for yes and 'cancel' for no.");
+
+  var num = window.confirm("Do you want numbers? Select 'ok' for yes and 'cancel' for no.");
+
+  var symbols = window.confirm("Do you want special characters? Select 'ok' for yes and 'cancel' for no.");
+
+
+  if (!lcase && !ucase && !num && !symbols) {
+    (prompt("You need to choose at least one option!"));
   };
-
-  getPasswordOptions();
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-
-
-
-
-
-
-
+  var answers = {
+    length: length,
+    lcase: lcase,
+    ucase: ucase,
+    num: num,
+    symbols: symbols,
   }
 
+  return answers;
 
 
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+
+
+
+
+}
+
+function generatePassword() {
+  var passwordOptions = getPasswordOptions();
+  console.log(passwordOptions);
+  var source = [];
+  var results = [];
+  if (passwordOptions.lcase) {
+    source = source.concat(alphabet);
+  }
+  if (passwordOptions.ucase) {
+    source = source.concat(ucAlphabet);
+  }
+  if (passwordOptions.num) {
+    source = source.concat(numbers);
+  }
+  if (passwordOptions.symbols) {
+    source = source.concat(special);
+  }
+  for (i = 0; i < passwordOptions.length; i++) {
+    var randomIndex = Math.floor(Math.random() * source.length);
+    results.push(source[randomIndex])
+  }
+  return results.join("");
+}
+
+
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+
+
+
+
+
+
+
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
